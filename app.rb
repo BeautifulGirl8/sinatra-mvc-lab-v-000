479806294a415e5a8d8748dfd60e32bd8438f6ea
Vicky  Tee <vttak9@gmail.com>
2021-04-08 05:37:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
   get '/' do
-  
+
       erb :user_input
     end
-  
+
     post '/piglatinize' do
       @input_phrase = params[:user_phrase]
       @output = PigLatinizer.new
-  
+
       erb :piglatinize
     end
-  
+
 end
